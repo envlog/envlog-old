094a2e24aa3f4ce7eefa5d7b022b29064332fb79
Stefano <stefano.mitello@gmail.com>
2021-11-30 14:53:45 +0100

    add firebase support

--- js/inc/login.js
+++ js/inc/login.js
@@ -16,14 +16,54 @@ const auth = firebase.auth()
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+function getUserProfileProvider() {
+    // [START auth_get_user_profile_provider]
+    const user = firebase.auth().currentUser;
+
+    if (user !== null) {
+        user.providerData.forEach((profile) => {
+            //           console.log("  Sign-in provider: " + profile.providerId);
+            //           console.log("  Provider-specific UID: " + profile.uid);
+            //           console.log("  Name: " + profile.displayName);
+            //           console.log("  Email: " + profile.email);
+            //           console.log("  Photo URL: " + profile.photoURL);
+
+            localStorage.setItem("auth-provider", profile.providerId);
+            localStorage.setItem("auth-uid", profile.uid);
+            localStorage.setItem("auth-displayName", profile.displayName);
+            localStorage.setItem("auth-email", profile.email);
+            localStorage.setItem("auth-photoURL", profile.photoURL);
+
+        });
+    }
+    // [END auth_get_user_profile_provider]
+}
 
 var uiConfig = {
     callbacks: {
         signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+
+            var user = authResult.user;
+            var credential = authResult.credential;
+            var isNewUser = authResult.additionalUserInfo.isNewUser; //ritorna bool se è nuovo utente registrato
+            var providerId = authResult.additionalUserInfo.providerId;
+            var operationType = authResult.operationType;
+
+
+
+
             // User successfully signed in.
             // Return type determines whether we continue the redirect automatically
             // or whether we leave that to developer to handle.
             return true;
+        },
+        signInFailure: function (error) {
+            // Some unrecoverable error occurred during sign-in.
+            // Return a promise when error handling is completed and FirebaseUI
+            // will reset, clearing any UI. This commonly occurs for error code
+            // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
+            // occurs. Check below for more details on this.
+            return handleUIError(error);
         },
         uiShown: function () {
             // The widget is rendered.
@@ -32,18 +72,20 @@ var uiConfig = {
         }
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInFlow: 'redirect',
+    signInSuccessUrl: '/dashboard.html',
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
+        firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
     //tosUrl: '<your-tos-url>',
     // Privacy policy url.
     //privacyPolicyUrl: '<your-privacy-policy-url>'
 };
+
+
 
 ui.start('#firebaseui-auth-container', uiConfig);
