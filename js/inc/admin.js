a421791365a4cda47d0a6f69fe8b8096099298f8
MarioCunsolo <mariocunsolo7@gmail.com>
2021-12-02 19:54:26 +0100

    added modal

--- js/inc/admin.js
+++ js/inc/admin.js
@@ -1,21 +1,60 @@
 // Graph
 var ctx = document.getElementById("myChart");
+var ctx1 = document.getElementById("myChart1");
 
 var myChart = new Chart(ctx, {
   type: "line",
   data: {
     labels: [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
+      "00:00",
+      "04:00",
+      "08:00",
+      "12:00",
+      "16:00",
+      "20:00",
     ],
     datasets: [
       {
-        data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+        data: [0, 15, 10, 5, 15, 28],
+        lineTension: 0,
+        backgroundColor: "transparent",
+        borderColor: "#007bff",
+        borderWidth: 4,
+        pointBackgroundColor: "#007bff",
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: false,
+          },
+        },
+      ],
+    },
+    legend: {
+      display: false,
+    },
+  },
+});
+
+var myChart1 = new Chart(ctx1, {
+  type: "line",
+  data: {
+    labels: [
+      "Lunedì",
+      "Martedì",
+      "Mercoledì",
+      "Giovedì",
+      "Venerdì",
+      "Sabato",
+      "Domenica",
+    ],
+    datasets: [
+      {
+        data: [0, 15, 10, 5, 15, 23, 31],
         lineTension: 0,
         backgroundColor: "transparent",
         borderColor: "#007bff",
