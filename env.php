90ecf3314ec79af84a011f9820032e8d8587b956
Stefano <stefano.mitello@gmail.com>
2021-12-05 22:29:23 +0100

    Create env.php

---
+++ env.php
@@ -0,0 +1,7 @@
+<?php
+
+define('ROOTPATH', dirname( __FILE__ ) . '/' );
+define('DOMAIN', $_SERVER['REQUEST_SCHEME'] .'://'. $_SERVER['HTTP_HOST'] . '/');
+
+define('APIDOMAIN', 'https://api.'.$_SERVER['HTTP_HOST'] . '/');
+
