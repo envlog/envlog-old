af554fd8e321af7c72ba1370fb85dafe8e674551
Stefano <stefano.mitello@gmail.com>
2021-12-05 22:29:44 +0100

    create partials components

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+       
+       
+       
+<!-- MDB -->
+<script type="text/javascript" src="<?=DOMAIN ?>js/mdb.min.js"></script>
+
+<!-- Custom scripts -->
+
+<script src="<?=DOMAIN ?>js/app.js"></script>
---
+++ inc/head.php
@@ -0,0 +1,22 @@
+<?php include('env.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+  <title>Home Dashboard</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Oxygen:wght@700&Roboto:wght@300;400;500;700&display=swap" />
+  <!-- MDB -->
+  <link rel="stylesheet" href="<?=DOMAIN; ?>css/mdb.free.css" />
+  <!-- Custom styles -->
+  <link rel="stylesheet" href="<?=DOMAIN ?>css/inc/navbar.css" /> 
+  <link rel="stylesheet" href="<?=DOMAIN ?>css/inc/dashboard.css" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw=="
+  crossorigin="anonymous"></script>
+</head>
