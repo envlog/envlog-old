3c7eeff9cf9b971c4e24b74645ae24623815d5d3
Stefano <stefano.mitello@gmail.com>
2021-12-06 22:29:21 +0100

    update

--- env.php
+++ env.php
@@ -3,5 +3,5 @@
 define('ROOTPATH', dirname( __FILE__ ) . '/' );
 define('DOMAIN', $_SERVER['REQUEST_SCHEME'] .'://'. $_SERVER['HTTP_HOST'] . '/');
 
-define('APIDOMAIN', 'https://api.'.$_SERVER['HTTP_HOST'] . '/');
+define('APIDOMAIN', 'https://api.envlog.mitello.xyz/');
 
